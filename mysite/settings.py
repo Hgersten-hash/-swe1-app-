1cb70420deb4e4a581849e295ed2e3481ecd059d
Hgersten-hash <70909790+Hgersten-hash@users.noreply.github.com>
2023-03-05 08:57:14 -0500

    Changes to .travis.yml

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 
 environ.Env.read_env()
-
-SECRET_KEY = env("SECRET_KEY")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,12 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
-
-project_folder = os.path.expanduser('/Users/hannahgersten/Desktop/NYU/Spring 2023/SWE/Django-Tutorial/mysite/mysite/')  # noqa E501
-load_dotenv(os.path.join(project_folder, '.env'))
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
