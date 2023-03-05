84ea56e7deefae146cd463e5a49e30d499e6ecd3
Hgersten-hash <70909790+Hgersten-hash@users.noreply.github.com>
2023-03-05 13:17:54 -0500

    AWS debugging

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    # "172.31.26.68",
+    "172.31.26.68",
     "django-polls-dev.us-east-1.elasticbeanstalk.com",
     "localhost",
 ]  # noqa E501
