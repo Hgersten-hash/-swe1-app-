46201a90bf00ddd94c1706e90901dc160673460d
Hgersten-hash <70909790+Hgersten-hash@users.noreply.github.com>
2023-03-05 09:46:14 -0500

    Reformatting

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env()
 
-
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
